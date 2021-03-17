2abd5f69a0a00146fe79e90a6cb2f7f9949b8e76
ruanshiron <sandt26@gmail.com>
2021-03-17 11:00:28 +0700

    ship it

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import prisma from "../../../lib/prisma";
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+  secret: process.env.SECRET,
+};
