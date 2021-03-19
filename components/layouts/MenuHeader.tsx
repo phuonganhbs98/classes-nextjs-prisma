603748b702f89229de6dff0fd28d009eab72be7a
ruanshiron <sandt26@gmail.com>
2021-03-19 23:21:29 +0700

    avatar

--- components/layouts/MenuHeader.tsx
+++ components/layouts/MenuHeader.tsx
@@ -3,6 +3,9 @@ import { useSession } from "next-auth/client";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const spinIcon = <LoadingOutlined style={{ fontSize: 30 }} spin />;
 
 type Props = {
   title: string;
@@ -18,17 +21,17 @@ const MenuHeader: React.FC<Props> = ({ title }) => {
     router.pathname === pathname;
 
   const auth = loading ? (
-    <a>
-      <Spin />
-    </a>
+    <div style={{ width: 44 }} key="1">
+      <Spin indicator={spinIcon} />
+    </div>
   ) : session ? (
-    <Link href="/profile">
+    <Link href="/profile" key="1">
       <a>
         <Avatar src={session?.user.image} />
       </a>
     </Link>
   ) : (
-    <Link href="/api/auth/signin">
+    <Link href="/api/auth/signin" key="1">
       <a data-active={isActive("/signup")}>Đăng nhập</a>
     </Link>
   );
@@ -42,7 +45,11 @@ const MenuHeader: React.FC<Props> = ({ title }) => {
         padding: 0,
       }}
     >
-      <PageHeader style={{ maxHeight: "64px" }} title={title} extra={[auth]} />
+      <PageHeader
+        style={{ maxHeight: "64px", padding: 12, paddingLeft: 24 }}
+        title={title}
+        extra={[auth]}
+      />
     </Header>
   );
 };
