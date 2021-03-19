291ef4f3bd6e4d676991c9bb06ec9cff1e874594
ruanshiron <sandt26@gmail.com>
2021-03-19 14:26:35 +0700

    Header with avatar

--- components/layouts/MenuHeader.tsx
+++ components/layouts/MenuHeader.tsx
@@ -1,34 +1,55 @@
-import { Avatar, Layout, Menu } from "antd";
+import { Avatar, Layout, Menu, Spin } from "antd";
+import { useSession } from "next-auth/client";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 type Props = {};
 
 const { Header } = Layout;
 
-const MenuHeader: React.FC<Props> = () => (
-  <Header
-    className="site-layout-background"
-    style={{
-      zIndex: 1,
-      width: "100%",
-    }}
-  >
-    <Menu mode="horizontal" style={{ lineHeight: "64px" }}>
-      <Menu.Item
-        style={{
-          float: "right",
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          width: "64px",
-        }}
-      >
-        <a>
-          <Avatar />
-        </a>
-      </Menu.Item>
-    </Menu>
-  </Header>
-);
+const MenuHeader: React.FC<Props> = () => {
+  const [session, loading] = useSession();
+
+  const router = useRouter();
+  const isActive: (pathname: string) => boolean = (pathname) =>
+    router.pathname === pathname;
+
+  return (
+    <Header
+      className="site-layout-background"
+      style={{
+        zIndex: 1,
+        width: "100%",
+      }}
+    >
+      <Menu mode="horizontal" style={{ lineHeight: "64px" }}>
+        <div
+          style={{
+            float: "right",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            width: "64px",
+          }}
+        >
+          {loading ? (
+            <a>
+              <Spin />
+            </a>
+          ) : session ? (
+            <a>
+              <Avatar src={session?.user.image} />
+            </a>
+          ) : (
+            <Link href="/api/auth/signin">
+              <a data-active={isActive("/signup")}>Đăng nhập</a>
+            </Link>
+          )}
+        </div>
+      </Menu>
+    </Header>
+  );
+};
 
 export default MenuHeader;
